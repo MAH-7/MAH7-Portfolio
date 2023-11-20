f3025a7082e9f0a09774c6d729ca0090dead993f
emanhakim <97828413+emanhakim@users.noreply.github.com>
2023-11-20 10:29:39 +0800

    Add-Client

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -3,35 +3,22 @@ import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 import "./About.scss";
+import { urlFor, client } from "../../client";
 
 const About = () => {
-  const abouts = [
-    {
-      title: "Web Development",
-      description: "I am a good developer.",
-      imgUrl: images.about01,
-    },
-    {
-      title: "Frontend Development",
-      description: "I am a good developer.",
-      imgUrl: images.about02,
-    },
-    {
-      title: "Backend Development",
-      description: "I am a good developer.",
-      imgUrl: images.about03,
-    },
-    {
-      title: "MERN Stack",
-      description: "I am a good developer.",
-      imgUrl: images.about04,
-    },
-  ];
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
       <h2 className="head-text">
-        I Know that <span>Good Design</span> <br />
+        I Know that <span>Good Apps</span> <br />
         means <span>Good Business</span>
       </h2>
 
@@ -44,7 +31,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
